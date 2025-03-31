30c8c87fad47c968271ddf1766f41aa218704f34
Ahmet <exarons@gmail.com>
2025-03-31 12:59:08 +0300

    home page login button style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/login">Login</Link>
+    <div className="flex justify-center items-center h-screen">
+      <Link href="/login" className="bg-orange-500 text-white px-4 py-2 rounded-md">Login</Link>
     </div>
   );
 }
